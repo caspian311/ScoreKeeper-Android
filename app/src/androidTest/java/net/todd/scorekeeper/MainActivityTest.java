9eae7a69ae4fc25522961b8e296991d47624e81e
Matt Todd <matt.c.todd@gmail.com>
2015-01-02 10:58:30 -0600

    first step in trying to get the tests running

---
+++ app/src/androidTest/java/net/todd/scorekeeper/MainActivityTest.java
@@ -0,0 +1,25 @@
+package net.todd.scorekeeper;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.assertThat;
+
+@RunWith(RobolectricTestRunner.class)
+public class MainActivityTest {
+    private MainActivity activity;
+    @Before
+    public void setup() throws Exception {
+        activity = Robolectric.buildActivity(MainActivity.class).create().get();
+    }
+
+    @Test
+    public void shouldHaveApplicationName() throws Exception {
+        String appName = activity.getResources().getString(R.string.app_name);
+        assertThat(appName, equalTo("MainActivity"));
+    }
+}
--- app/src/androidTest/java/net/todd/scorekeeper/UriTest.java
+++ app/src/androidTest/java/net/todd/scorekeeper/UriTest.java
@@ -5,14 +5,22 @@ import android.net.Uri;
 
 import junit.framework.TestCase;
 
-public class UriTest extends TestCase {
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+
+import static junit.framework.Assert.assertEquals;
+
+@RunWith(RobolectricTestRunner.class)
+public class UriTest {
     private static final int GAMES = 100;
     private static final int GAME = 101;
     private static final int SCOREBOARDS = 102;
 
     private UriMatcher uriMatcher;
 
-    @Override
+    @Before
     public void setUp() {
         uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
         uriMatcher.addURI("net.todd.scorekeeper", "games", GAMES);
@@ -20,12 +28,14 @@ public class UriTest extends TestCase {
         uriMatcher.addURI("net.todd.scorekeeper", "games/#/scoreboard", SCOREBOARDS);
     }
 
+    @Test
     public void testMatchers() {
         assertEquals(GAMES, uriMatcher.match(Uri.parse("content://net.todd.scorekeeper/games")));
         assertEquals(GAME, uriMatcher.match(Uri.parse("content://net.todd.scorekeeper/games/123")));
         assertEquals(SCOREBOARDS, uriMatcher.match(Uri.parse("content://net.todd.scorekeeper/games/123/scoreboard")));
     }
 
+    @Test
     public void testSegments() {
         Uri gameUri = Uri.parse("content://net.todd.scorekeeper/games/123");
         Uri scoreboardUri = Uri.parse("content://net.todd.scorekeeper/games/123/scoreboard");
@@ -34,6 +44,7 @@ public class UriTest extends TestCase {
         assertEquals("123", scoreboardUri.getPathSegments().get(1));
     }
 
+    @Test
     public void testAddingToAUri() {
         Uri gamesUri = Uri.parse("content://net.todd.scorekeeper/games");
         Uri gameUri = Uri.parse("content://net.todd.scorekeeper/games/123");
